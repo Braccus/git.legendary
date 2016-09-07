6ff2d6aa965340fd4268c6f16dc4d115928c1a90
Vitaly <vboltunov@gmail.com>
2016-09-07 16:55:56 +0300

    started developing class "product"

---
+++ controllers/Product.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Class Products
+ */
+class Products extends Controller
+{
+  /** @var array $allowedActions См. определение массива в базовом, Controller классе. */
+  protected $allowedActions = ['index'];
+
+  /**
+   * Просто пример страницы по умолчанию доступной как главная, то есть http://site/
+   */
+  protected function actionIndex()
+  {
+    $product = ProductProperties::getProductProperties();
+    $this->templater->assign('product', $product);
+    $this->layoutVars['title'] = $product['title'];
+    $this->view = 'product';
+  }
+}
