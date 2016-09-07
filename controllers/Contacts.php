45ec2fb1db1ee213b2e26fc4a023e211b83a25a8
Vitaly <vboltunov@gmail.com>
2016-09-07 15:38:11 +0300

    finished contacts page

--- controllers/Contacts.php
+++ controllers/Contacts.php
@@ -5,5 +5,17 @@
  */
 class Contacts extends Controller
 {
+  /** @var array $allowedActions См. определение массива в базовом, Controller классе. */
+  protected $allowedActions = ['index'];
 
+  /**
+   * Экшен отображения страницы контактов.
+   */
+  protected function actionIndex()
+  {
+    $contacts = ContactsItems::getContactsItems();
+    $this->templater->assign('contacts', $contacts);
+    $this->layoutVars['title'] = 'Контакты';
+    $this->view = 'contacts';
+  }
 }
---
+++ models/ContactsItems.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Class ContactsItems
+ */
+class ContactsItems extends Model
+{
+  /** @var array $data См. Model $data */
+  protected $data = [];
+  /** @var array $properties См. Model $properties */
+  protected static $properties = ['phone', 'address', 'map'];
+
+  public static function getContactsItems()
+  {
+    //Достаем данные из таблицы contacts
+    $data = Db::getInstance()
+      ->Select('SELECT ' . implode(',', self::$properties) . ' FROM contacts');
+    // Формируем массив, который будет содержать объекты класса ContactsItems с данными внутри и возвращаем этот массив.
+    $contacts = new ContactsItems();
+    $contacts->data = $data[0];
+    return $contacts;
+
+  }
+
+  /**
+   * Геттер phone.
+   * @return string
+   */
+  public function getPhone()
+  {
+    return $this->data['phone'];
+  }
+
+  /**
+   * Геттер address.
+   * @return string
+   */
+  public function getAddress()
+  {
+    return $this->data['address'];
+  }
+
+  /**
+   * Геттер map.
+   * @return string
+   */
+  public function getMap()
+  {
+    return $this->data['map'];
+  }
+}
+
