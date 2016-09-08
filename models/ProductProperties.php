b1536e360ebc7ac6107b410f7b9b7ef8cbd8efea
Braccus <vboltunov@gmail.com>
2016-09-09 00:54:25 +0300

    deleted unused method getAllProductIds()

--- models/ProductProperties.php
+++ models/ProductProperties.php
@@ -30,13 +30,6 @@ class ProductProperties extends Model
     return $productSpecs;
   }
 
-  public static function getAllProductIds()
-  {
-    //Достаем данные из таблицы products
-    $ids = Db::getInstance()
-      ->Select('SELECT id FROM products');
-  }
-
   /**
    * Геттер id.
    * @return string
